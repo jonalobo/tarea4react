0594cde02006c30da24754e19285c91f9eab154f
Jonathan Alvarado <jonalobo98@gmail.com>
2022-04-26 00:11:59 -0600

    contador

--- src/components/itemcount/ItemCount.js
+++ src/components/itemcount/ItemCount.js
@@ -1,18 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-const ItemCount = ({stock,initial,onAdd}) => {
+const ItemCount = ({initial, stock}) => {
 
   const [inventario, setInventario] = useState(initial)
+  
+  useEffect(() => {
+    console.log(stock)
+  }, [])
+  
 
 
   return (
     <div>
         <div tabIndex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
         <div className="card-body">
-          <span className="font-bold text-lg">{initial}</span>
+          <span className="font-bold text-lg">{inventario}</span>
           
           <div className="card-actions">
-            <button className="btn btn-primary">+</button>
+            <button onClick={()=>{setInventario(inventario + 1)}} className="btn btn-primary">+</button>
             <button className="btn btn-primary">-</button>
           </div>
         </div>
--- src/components/itemlistcontainer/ItemListContainer.js
+++ src/components/itemlistcontainer/ItemListContainer.js
@@ -3,7 +3,7 @@ import ItemCount from "../itemcount/ItemCount"
 const ItemListContainer = () => {
   return (
     <div style={{width:'100%',height:'100vh',backgroundColor:'white',display:'flex',justifyContent:'center'}}>
-        <ItemCount />
+        <ItemCount stock={6} initial={1}/>
     </div>
   )
 }
